4bf8a74d260775f8f03e361f5a9fc46f55b76ac7
andrealenzi11@gmail.com <andrealenzi11@gmail.com>
2022-02-21 01:44:54 +0100

    noise generation with CTGAN

--- core/noise_generation.py
+++ core/noise_generation.py
@@ -1,7 +1,8 @@
+import math
 from typing import Tuple, List
 
 import pandas as pd
-from sdv.tabular import GaussianCopula
+from sdv.tabular import CTGAN
 
 from config import HEAD, RELATION, TAIL
 from dao.data_model import NoisyDataset
@@ -13,28 +14,43 @@ class NoiseGenerator:
                  training_df: pd.DataFrame,
                  validation_df: pd.DataFrame,
                  testing_df: pd.DataFrame):
-        self.training_df = training_df.reset_index(drop=True)
-        self.validation_df = validation_df.reset_index(drop=True)
-        self.testing_df = testing_df.reset_index(drop=True)
-        self.model = GaussianCopula()
+        self.training_df = training_df[[HEAD, RELATION, TAIL]].astype("str").reset_index(drop=True)  # "category"
+        self.validation_df = validation_df[[HEAD, RELATION, TAIL]].astype("str").reset_index(drop=True)  # "category"
+        self.testing_df = testing_df[[HEAD, RELATION, TAIL]].astype("str").reset_index(drop=True)  # "category"
+        self.model = CTGAN(
+            field_names=[HEAD, RELATION, TAIL],
+            field_types={
+                HEAD: {'type': 'categorical'},
+                RELATION: {'type': 'categorical'},
+                TAIL: {'type': 'categorical'},
+            },
+            verbose=True,
+            cuda=True,
+            batch_size=500,  # 500
+            epochs=300  # 300
+        )
+        self.is_fitted = False
 
     def train(self):
-        print("\n\t\t - Start Fit...")
-        df_x = self.training_df.sample(100)
-        df_x[HEAD] = df_x[HEAD].astype("str")
-        df_x[RELATION] = df_x[RELATION].astype("str")
-        df_x[TAIL] = df_x[TAIL].astype("str")
-        df_x = df_x.reset_index(drop=True)
-        print(df_x)
-        print(df_x.info())
-        self.model.fit(df_x)
+        print("\n\t\t - Start Fitting on Data ...")
+        training_sample_df = self.training_df.copy().sample(500).astype("str").reset_index(drop=True)
+        self.model.fit(data=training_sample_df)
+        self.is_fitted = True
+        print("\t\t - End fitting! \n")
+
+    def store_model(self):
+        self.model.save('my_model.pkl')
+
+    def load_model(self):
+        self.model = CTGAN.load('my_model.pkl')
+        self.is_fitted = True
 
     def _generate_noise(self,
                         noise_percentage: int,
                         partition_name: str,
                         partition_df: pd.DataFrame) -> Tuple[pd.DataFrame, List[int]]:
         partition_original_size = partition_df.shape[0]
-        partition_sample_size = int(partition_original_size / 100 * noise_percentage)
+        partition_sample_size = int(math.ceil(partition_original_size / 100 * noise_percentage))
         print(f"[noise_{noise_percentage}%] {partition_name}_sample_size: {partition_sample_size}")
         partition_anomalies_df = self.model.sample(num_rows=partition_sample_size).reset_index(drop=True)
         partition_final_df = pd.concat([partition_df, partition_anomalies_df],
@@ -45,6 +61,8 @@ class NoiseGenerator:
         return partition_final_df, partition_fake_y
 
     def generate_noisy_dataset(self, noise_percentage: int) -> NoisyDataset:
+        if not self.is_fitted:
+            raise Exception("Error: the CTGAN model is not already fitted on training data!")
         training_final_df, training_fake_y = self._generate_noise(noise_percentage=noise_percentage,
                                                                   partition_name="training",
                                                                   partition_df=self.training_df)
--- scripts/exporting_noisy_datasets_to_tsv_files.py
+++ scripts/exporting_noisy_datasets_to_tsv_files.py
@@ -2,8 +2,10 @@ from core.noise_generation import NoiseGenerator
 from dao.data_model import DatasetName, NoiseLevel
 from dao.dataset_loading import TsvDatasetLoader
 
+
 if __name__ == '__main__':
-    tsv_dataset_loader = TsvDatasetLoader(dataset_name=DatasetName.COUNTRIES.value,
+
+    tsv_dataset_loader = TsvDatasetLoader(dataset_name=DatasetName.YAGO310.value,
                                           noise_level=NoiseLevel.ZERO.value)
 
     df_training, df_validation, df_testing = tsv_dataset_loader.get_training_validation_testing_dfs()
@@ -15,7 +17,20 @@ if __name__ == '__main__':
                                      validation_df=df_validation,
                                      testing_df=df_testing)
     noise_generator.train()
-    noisy_dataset = noise_generator.generate_noisy_dataset(noise_percentage=1)
+    # noise_generator.store_model()
+    noisy_dataset = noise_generator.generate_noisy_dataset(noise_percentage=5)
+
+    print("\n\n ### NOISY TRAINING ###")
     print(noisy_dataset.training_df.shape)
+    print(len(noisy_dataset.training_fake_y))
+    print(noisy_dataset.training_df)
+
+    print("\n\n ### NOISY VALIDATION ###")
     print(noisy_dataset.validation_df.shape)
+    print(len(noisy_dataset.validation_fake_y))
+    print(noisy_dataset.validation_df)
+
+    print("\n\n ### NOISY TESTING ###")
     print(noisy_dataset.testing_df.shape)
+    print(len(noisy_dataset.testing_fake_y))
+    print(noisy_dataset.testing_df)
